a44455e029a2de96db3aca7afdbc6f4f08ac7b44
k-holy <k.holy74@gmail.com>
2012-06-12 00:06:03 +0900

    refactoring

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,10 +7,15 @@
  * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License (MIT)
  */
 spl_autoload_register(function($className) {
-	if (0 === strpos(ltrim($className, '/'), 'Volcanus\Error')) {
-		require_once realpath(__DIR__ . '/..') . substr(
-			str_replace('\\', '/', $className),
-			strlen('Volcanus\Error')
+	$namespace = 'Volcanus\Error';
+	if (0 === strpos(ltrim($className, DIRECTORY_SEPARATOR), $namespace)) {
+		$path = realpath(__DIR__ . '/..') . substr(
+			str_replace('\\', DIRECTORY_SEPARATOR, $className),
+			strlen($namespace)
 		).'.php';
+		if (file_exists($path)) {
+			return include $path;
+		}
 	}
+	return false;
 }, true, true);
