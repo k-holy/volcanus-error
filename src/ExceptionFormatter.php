43bee767de799ebdd1bc93ec8720a485ed103dcc
k-holy <k.holy74@gmail.com>
2017-09-26 14:18:07 +0900

    コーディングスタイルをPSR-2で整形

--- src/ExceptionFormatter.php
+++ src/ExceptionFormatter.php
@@ -16,45 +16,45 @@ namespace Volcanus\Error;
 class ExceptionFormatter implements ExceptionFormatterInterface
 {
 
-	/**
-	 * 例外オブジェクトを文字列に整形して返します。
-	 *
-	 * @param \Exception 例外オブジェクト
-	 * @return string
-	 */
-	public function __invoke(\Exception $e)
-	{
-		return $this->format($e);
-	}
+    /**
+     * 例外オブジェクトを文字列に整形して返します。
+     *
+     * @param \Exception 例外オブジェクト
+     * @return string
+     */
+    public function __invoke(\Exception $e)
+    {
+        return $this->format($e);
+    }
 
-	/**
-	 * 例外オブジェクトを文字列に整形して返します。
-	 *
-	 * @param \Exception 例外オブジェクト
-	 * @return string
-	 */
-	public function format(\Exception $e)
-	{
-		return sprintf("%s '%s' in %s on line %u",
-			$this->buildHeader($e),
-			$e->getMessage(),
-			$e->getFile(),
-			$e->getLine()
-		);
-	}
+    /**
+     * 例外オブジェクトを文字列に整形して返します。
+     *
+     * @param \Exception 例外オブジェクト
+     * @return string
+     */
+    public function format(\Exception $e)
+    {
+        return sprintf("%s '%s' in %s on line %u",
+            $this->buildHeader($e),
+            $e->getMessage(),
+            $e->getFile(),
+            $e->getLine()
+        );
+    }
 
-	/**
-	 * 例外からエラーメッセージ用のヘッダを生成して返します。
-	 *
-	 * @param \Exception 例外オブジェクト
-	 * @return string
-	 */
-	public function buildHeader(\Exception $e)
-	{
-		return sprintf("Uncaught Exception %s[%d]:",
-			get_class($e),
-			$e->getCode()
-		);
-	}
+    /**
+     * 例外からエラーメッセージ用のヘッダを生成して返します。
+     *
+     * @param \Exception 例外オブジェクト
+     * @return string
+     */
+    public function buildHeader(\Exception $e)
+    {
+        return sprintf("Uncaught Exception %s[%d]:",
+            get_class($e),
+            $e->getCode()
+        );
+    }
 
 }
